348f27e8f379ce728bbde23f22283a4a0f0b920a
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-24 04:03:15 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let result = time.split(' ').parseInt()
+  if (result < 12) {
+    return "Good Morning"
+  } else if (result > 12 && result < 17) {
+    return "Good Afternoon"
+  } else if (result > 17) {
+  return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage (text) {
+  let greeting = document.getElementById('greeting')
+  greeting.write(text)
+}
