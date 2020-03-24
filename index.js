d1e1287308daaa5f1d103ae1ed2ac5415568390f
ctdupuis <ctdupuis7@gmail.com>
2020-03-23 21:08:44 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = Number(time.split(":")[0]);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
+}
