f740474fb05b78b1b73c8e7abdbaff8fc3dda331
Sharon Watkins <swatkins876@gmail.com>
2020-03-24 17:58:12 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string) {
+  if (parseInt(string, 10) < 12) {
+    return "Good Morning"
+  }
+  else if (parseInt(string, 10) >17) {
+    return "Good Evening"
+  }
+  else {
+      return "Good Afternoon"
+  }
+}
+
+function displayMessage(testContent) {
+  document.getElementById("greeting").innerText = testContent
+}
