dc317e27c7d527443153055fa55bea990f3e2d28
fasnimo <fasnimo@gmail.com>
2020-04-02 23:39:47 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeNow) {
+  let time = parseInt(timeNow)
+  if (time < 12) {
+    return `Good Morning`;
+  } else if (time >= 12 && time < 17) {
+    return `Good Afternoon`;
+  } else {
+    return `Good Evening`
+  }
+};
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(testContent) {
+  let wordZ = document.getElementById('greeting');
+  wordZ.innerText = testContent; 
+}
